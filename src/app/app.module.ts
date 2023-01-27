3492bf5ed2d66ca57796ccba5de97c6f8a00075a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2023-01-27 15:47:29 -0300

    add services component to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SliderMainComponent } from './components/slider-main/slider-main.component';
 import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './components/about/about.component';
     BannerComponent,
     HeaderComponent,
     SliderMainComponent,
-    AboutComponent
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
