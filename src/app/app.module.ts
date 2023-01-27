6835f2fce1e313505d0e51a37c9ddc51d8cb52f5
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2023-01-27 13:39:33 -0300

    add header component to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
