6d137414bc6b8f068431b27135cbc702942c26b8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2023-01-27 15:38:12 -0300

    add slider-main component to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SliderMainComponent } from './components/slider-main/slider-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    HeaderComponent
+    HeaderComponent,
+    SliderMainComponent
   ],
   imports: [
     BrowserModule,
